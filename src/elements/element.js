b2e90f1b82f8f15e2d9e325b5cc1783c24147a93
chong.john716 <chong.john716@gmail.com>
2018-10-03 18:46:52 +0800

    lint

--- src/elements/element.js
+++ src/elements/element.js
@@ -4,50 +4,50 @@ function Element() {
 }
 
 Element.prototype.initialize = function () {
-	this.elementType = 'element',
-	this.serializedProperties = ['elementType'],
-	this.propertiesStack = [],
-	this._id = '' + (new Date()).getTime()
+  this.elementType = 'element',
+  this.serializedProperties = ['elementType'],
+  this.propertiesStack = [],
+  this._id = '' + (new Date()).getTime()
 }
 
 Element.prototype.distroy = noop
 Element.prototype.removeHandler = noop
 
 Element.prototype.attr = function (a, b) {
-	if (a != null && b != null) { this[a] = b } else if (a != null) { return this[a] }
-	return this
+  if (a != null && b != null) { this[a] = b } else if (a != null) { return this[a] }
+  return this
 }
 
 Element.prototype.save = function () {
-	var a = this,
-		b = {}
-	this.serializedProperties.forEach(function (c) {
-		b[c] = a[c]
-	}),
-	this.propertiesStack.push(b)
+  var a = this,
+    b = {}
+  this.serializedProperties.forEach(function (c) {
+    b[c] = a[c]
+  }),
+  this.propertiesStack.push(b)
 }
 
 Element.prototype.restore = function () {
-	if (this.propertiesStack != null && this.propertiesStack.length != 0) {
-		var a = this,
-			b = this.propertiesStack.pop()
-		this.serializedProperties.forEach(function (c) {
-			a[c] = b[c]
-		})
-	}
+  if (this.propertiesStack != null && this.propertiesStack.length != 0) {
+    var a = this,
+      b = this.propertiesStack.pop()
+    this.serializedProperties.forEach(function (c) {
+      a[c] = b[c]
+    })
+  }
 }
 
 Element.prototype.toJson = function () {
-	var a = this,
-		b = '{',
-		c = this.serializedProperties.length
-	return this.serializedProperties.forEach(function (d, e) {
-		var f = a[d]
-		typeof f === 'string' && (f = '"' + f + '"'),
-		b += '"' + d + '":' + f,
-		c > e + 1 && (b += ',')
-	}),
-	b += '}'
+  var a = this,
+    b = '{',
+    c = this.serializedProperties.length
+  return this.serializedProperties.forEach(function (d, e) {
+    var f = a[d]
+    typeof f === 'string' && (f = '"' + f + '"'),
+    b += '"' + d + '":' + f,
+    c > e + 1 && (b += ',')
+  }),
+  b += '}'
 }
 
 export default Element
--- src/message-bus.js
+++ src/message-bus.js
@@ -1,34 +1,34 @@
 
 function MessageBus(a) {
-	this.name = a
-	this.messageMap = {}
-	this.messageCount = 0
+  this.name = a
+  this.messageMap = {}
+  this.messageCount = 0
 }
 
 MessageBus.prototype.subscribe = function (a, c) {
-	var d = this.messageMap[a]
-	d == null && (this.messageMap[a] = [])
-	this.messageMap[a].push(c)
-	this.messageCount++
+  var d = this.messageMap[a]
+  d == null && (this.messageMap[a] = [])
+  this.messageMap[a].push(c)
+  this.messageCount++
 }
 
 MessageBus.prototype.unsubscribe = function (a) {
-	var c = this.messageMap[a]
-	c != null && (this.messageMap[a] = null, delete this.messageMap[a], this.messageCount--)
+  var c = this.messageMap[a]
+  c != null && (this.messageMap[a] = null, delete this.messageMap[a], this.messageCount--)
 }
 
 MessageBus.prototype.publish = function (a, c, d) {
-	var e = this.messageMap[a]
-	if (e != null) {
-		for (var f = 0; f < e.length; f++) {
-			d ? !(function (a, b) {
+  var e = this.messageMap[a]
+  if (e != null) {
+    for (var f = 0; f < e.length; f++) {
+      d ? !(function (a, b) {
         setTimeout(function () {
           a(b)
         }, 10)
       }
-        (e[f], c)) : e[f](c)
-		}
-	}
+      (e[f], c)) : e[f](c)
+    }
+  }
 }
 
 export default MessageBus
